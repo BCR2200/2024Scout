183efd18cbd8eddc196aa72693071c665441c367
Cerulity64X <cerulity32k@gmail.com>
2024-03-07 16:57:51 -0500

    crescendo!!!!!!!!!

--- lib/main.dart
+++ lib/main.dart
@@ -37,11 +37,63 @@ class MyHomePage extends StatefulWidget {
 abstract class JStat {
   void reset();
   String? get();
-  Widget? widget();
+  Widget? widget(BuildContext context);
   void setUpdate(void Function(void Function()) fn) {}
   void update() {}
 }
+class StringInput extends JStat {
+  TextEditingController txt = TextEditingController();
+  String name;
+  StringInput(this.name);
 
+  late void Function(void Function()) refresh;
+  @override
+  void setUpdate(void Function(void Function()) fn) {
+    refresh = fn;
+  }
+
+  @override
+  void reset() {
+    txt.text = "";
+  }
+
+  @override
+  void update() {}
+
+  @override
+  String? get() => txt.text;
+
+  @override
+  Widget widget(BuildContext context) {
+    Widget field =
+    TextField(
+      controller: txt,
+    );
+
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      
+      children: [
+        Expanded(
+          flex: 1,
+          child: Text(
+            name,
+            textAlign: TextAlign.center
+          ),
+        ),
+        
+        Expanded(
+          flex: 2,
+          child: Row(
+            children: [
+              Expanded(child: field),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
 class Numeric extends JStat {
   BigInt? num;
   BigInt? max;
@@ -49,7 +101,7 @@ class Numeric extends JStat {
   TextEditingController txt = TextEditingController();
 
   String name;
-  Numeric(this.name, {this.max = null});
+  Numeric(this.name, {this.max});
 
   late void Function(void Function()) refresh;
   @override
@@ -72,7 +124,7 @@ class Numeric extends JStat {
   String? get() => num?.toString() ?? "0";
 
   @override
-  Widget widget() {
+  Widget widget(BuildContext context) {
     Widget field =
     TextField(
       onChanged: (value) {
@@ -106,7 +158,7 @@ class Numeric extends JStat {
           flex: 2,
           child: Row(
             children: [
-              TextButton(
+              Expanded(child: TextButton(
                 onPressed: () {
                   if (num == null) {
                     num = BigInt.one;
@@ -115,10 +167,10 @@ class Numeric extends JStat {
                   }
                   txt.text = num?.toString() ?? "";
                 },
-                child: const Text("+"),
-              ),
+                child: Text("+", style: Theme.of(context).textTheme.headlineLarge,),
+              ),),
               Expanded(child: field),
-              TextButton(
+              Expanded(child: TextButton(
                 onPressed: () {
                   if (num == null) {
                     num = BigInt.zero;
@@ -127,8 +179,8 @@ class Numeric extends JStat {
                   }
                   txt.text = num?.toString() ?? "";
                 },
-                child: const Text("-"),
-              ),
+                child: Text("-", style: Theme.of(context).textTheme.headlineLarge),
+              ),),
             ],
           ),
         ),
@@ -143,13 +195,11 @@ class Boolean extends JStat {
   Boolean(this.name);
 
   late void Function(void Function()) refresh;
-  @override
-  void setUpdate(void Function(void Function()) fn) {
+  @override void setUpdate(void Function(void Function()) fn) {
     refresh = fn;
   }
 
-  @override
-  void reset() {
+  @override void reset() {
     enabled = false;
   }
 
@@ -157,7 +207,7 @@ class Boolean extends JStat {
   String? get() => enabled ? "1" : "0";
 
   @override
-  Widget widget() {
+  Widget widget(BuildContext context) {
     var cb = Checkbox(
       value: enabled,
       onChanged: (value) {
@@ -176,6 +226,70 @@ class Boolean extends JStat {
         ),
         
         Expanded(child: cb,),
+      ],
+    );
+  }
+}
+class Team extends JStat {
+  String? team;
+
+  String name;
+  Team(this.name);
+
+  late void Function(void Function()) refresh;
+  @override
+  void setUpdate(void Function(void Function()) fn) {
+    refresh = fn;
+  }
+
+  @override
+  void reset() {
+    team = "Red";
+  }
+
+  @override
+  String? get() => team;
+
+  @override
+  Widget widget(BuildContext context) {
+    var radio = <Widget>[
+      Row(
+        children: [
+          const Expanded(child: Text("Red"),),
+          Expanded(child: Radio<String>(
+            value: "Red",
+            groupValue: team,
+            onChanged: (value) {
+              refresh(() { team = value; });
+            },
+          ),),
+        ],
+      ),
+      Row(
+        children: [
+          const Expanded(child: Text("Blue"),),
+          Expanded(child: Radio<String>(
+            value: "Blue",
+            groupValue: team,
+            onChanged: (value) {
+              refresh(() { team = value; });
+            },
+          ),),
+        ],
+      ),
+    ];
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      
+      children: [
+        Expanded(
+          child: Text(
+            name,
+            textAlign: TextAlign.center
+          ),
+        ),
+        
+        Expanded(child: Column(children: radio,),),
       ],
     );
   }
@@ -216,7 +330,7 @@ class MultipleFlag extends JStat {
   }
 
   @override
-  Widget widget() {
+  Widget widget(BuildContext context) {
     var radio = <Widget>[];
     for (var i = 0; i < variants.length; i++) {
       radio.add(
@@ -272,7 +386,7 @@ class MultipleCount extends JStat {
   String? get() => (ticked ?? 0 + 1).toString();
 
   @override
-  Widget widget() {
+  Widget widget(BuildContext context) {
     var radio = <Widget>[];
     for (var i = 0; i < variants.length; i++) {
       radio.add(
@@ -342,7 +456,7 @@ class ColorBox extends JStat {
   }
 
   @override
-  Widget widget() {
+  Widget widget(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(padding),
       child: Container(
@@ -353,7 +467,7 @@ class ColorBox extends JStat {
         margin: EdgeInsets.all(padding),
         child: Column(
           children: stats
-            .map((s) => s.widget())
+            .map((s) => s.widget(context))
             .where((element) => element != null)
             .map((e) => e!)
             .toList(),
@@ -373,7 +487,7 @@ class Heading extends JStat {
   void reset() {}
 
   @override
-  Widget widget() => Center(child: Text(text, textScaleFactor: 2.0));
+  Widget widget(BuildContext context) => Center(child: Text(text, textScaleFactor: 2.0));
 }
 class Calculated extends JStat {
   String? Function() calcFn;
@@ -383,23 +497,113 @@ class Calculated extends JStat {
   @override
   void reset() {}
   @override
-  Widget? widget() => null;
+  Widget? widget(BuildContext context) => null;
+}
+
+class Notemap extends JStat {
+  List<bool> enableds = [];
+  int count;
+  int start;
+  var checkboxes = <Widget>[];
+  bool flip = false;
+
+  String name;
+  Notemap(this.name, this.count, this.start);
+
+  late void Function(void Function()) refresh;
+  @override
+  void setUpdate(void Function(void Function()) fn) {
+    refresh = fn;
+  }
+
+  void setFlip(bool flip) {
+    if (this.flip ^ flip) {
+      checkboxes = checkboxes.reversed.toList();
+    }
+    this.flip = flip;
+  }
+
+  @override
+  void reset() {
+    for (var i = 0; i < count; i++) {
+      enableds[i] = false;
+    }
+  }
+
+  @override
+  String? get() {
+    var iter = flip ? enableds.reversed : enableds;
+    return iter
+    .map((e) => e ? "1" : "0")
+    .join(DELIMITER);
+  }
+
+  @override
+  Widget widget(BuildContext context) {
+    while (enableds.length < count) {
+      enableds.add(false);
+    }
+    checkboxes = <Widget>[];
+    for (var i = 0; i < count; i++) {
+      var cb = Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.orange.withOpacity(0.5),
+            borderRadius: BorderRadius.circular(10.0)
+          ),
+          margin: const EdgeInsets.all(5.0),
+          child: Column(
+            children: [
+              Checkbox(
+                value: enableds[i],
+                onChanged: (value) {
+                  refresh(() {enableds[i] = (value ?? false);});
+                },
+              ),
+            ],
+          ),
+        ),
+      );
+      checkboxes.add(cb);
+    }
+    return Column(children: [
+      Text(name),
+      Row(
+        mainAxisSize: MainAxisSize.min,
+        children: checkboxes
+      ),
+    ],);
+  }
 }
 
 class QRView extends StatelessWidget {
   final String data;
-  const QRView(this.data, {super.key});
+  final String teamNumber;
+  const QRView(this.data, this.teamNumber, {super.key});
 
   @override
   Widget build(BuildContext context) {
     print(data);
     return Scaffold(
       appBar: AppBar(title: const Text("QR Code")),
-      body: QrImageView(
-        data: data,
-        version: QrVersions.auto,
-        size: 1280,
-        gapless: true
+      body: Column(
+        children: [
+          Text(
+            teamNumber,
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          Text(data),
+          Expanded(
+            child: Center(
+              child: QrImageView(
+                data: data,
+                version: QrVersions.auto,
+                gapless: true
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -440,7 +644,7 @@ class BottomUpGrid extends JStat {
   }
 
   @override
-  Widget widget() {
+  Widget widget(BuildContext context) {
     var chunks = [];
     for (var i = 0; i < stats.length; i += verticalCount) {
       chunks.add(stats.sublist(i, clampc(i + verticalCount, stats.length)));
@@ -449,7 +653,7 @@ class BottomUpGrid extends JStat {
     for (List<JStat> statColumn in chunks) {
       List<Widget> columnWidgets = [];
       for (var jstat in statColumn) {
-        var widget = jstat.widget();
+        var widget = jstat.widget(context);
         if (widget != null) {
           columnWidgets.insert(0, widget);
         }
@@ -460,9 +664,60 @@ class BottomUpGrid extends JStat {
   }
 }
 
+class Hidden extends JStat {
+  JStat stat;
+  Hidden(this.stat);
+  @override
+  String? get() {
+    return stat.get();
+  }
+
+  @override
+  void reset() {
+    stat.reset();
+  }
+
+  @override
+  void setUpdate(void Function(void Function()) fn) {
+    stat.setUpdate(fn);
+  }
+
+  @override
+  void update() {
+    stat.update();
+  }
+
+  @override Widget? widget(BuildContext context) => null;
+}
+class OnlyShow extends JStat {
+  JStat stat;
+  OnlyShow(this.stat);
+  @override
+  String? get() => null;
+
+  @override
+  void reset() {
+    stat.reset();
+  }
+
+  @override
+  void setUpdate(void Function(void Function()) fn) {
+    stat.setUpdate(fn);
+  }
+
+  @override
+  void update() {
+    stat.update();
+  }
+
+  @override Widget? widget(BuildContext context) => stat.widget(context);
+}
+
+const COLORBOX_OPACITY = 0.25;
 class _MyHomePageState extends State<MyHomePage> {
-  var teleopPieces = <JStat>[];
-  var statistics = <JStat>[];
+  late Numeric teamNum;
+  late List<JStat> statistics;
+  bool initialized = false;
 
   void update(void Function() fn) {
     setState(fn);
@@ -471,9 +726,8 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  @override
-  Widget build(BuildContext context) {
-    teleopPieces = <JStat>[
+  void loadChargedUp() {
+    var teleopPieces = <JStat>[
       ColorBox([Numeric("L1 Cone")], Colors.yellow.withAlpha(48)),
       ColorBox([Numeric("L2 Cone")], Colors.yellow.withAlpha(88)),
       ColorBox([Numeric("L3 Cone")], Colors.yellow.withAlpha(128)),
@@ -537,11 +791,123 @@ class _MyHomePageState extends State<MyHomePage> {
         Colors.blue.withAlpha(64),
       ),
     ];
+  }
 
+  void loadCrescendo() {
+    // Bindings for NoCalc/Hidden variables.
+    var middleNotes = Notemap("Middle Note Pickups", 5, 4);
+    var teamsideNotes = Notemap("Teamside Note Pickups", 3, 1);
+    var team = Team("Team");
+
+    var teleopSpeaker = Numeric("Speaker Scores");
+    var teleopAmp = Numeric("Amp Scores");
+    var teleopAmpSpeaker = Numeric("Amplified Speaker Scores");
+
+    var autoSpeaker = Numeric("Speaker Scores");
+    var autoAmp = Numeric("Amp Scores");
+
+    var mobility = Boolean("Mobility");
+
+    var park = Boolean("Park");
+    var climb = Boolean("Climb");
+    var trap = Numeric("Trap Scores");
+
+    teamNum = Numeric("Team Number");
+    statistics = <JStat>[
+      ColorBox(
+        [
+          Heading("Metadata"),
+          teamNum,
+          Numeric("Match Number"),
+          OnlyShow(team),
+        ],
+        Colors.yellow.withOpacity(COLORBOX_OPACITY),
+      ),
+
+      ColorBox(
+        [
+          Heading("Autonomous"),
+          mobility,
+          autoSpeaker,
+          autoAmp,
+          OnlyShow(middleNotes),
+          OnlyShow(teamsideNotes),
+        ],
+        Colors.red.withOpacity(COLORBOX_OPACITY),
+      ),
+
+      ColorBox(
+        [
+          Heading("Teleop"),
+          teleopSpeaker,
+          teleopAmp,
+          ColorBox(
+            [teleopAmpSpeaker,],
+            const Color(0xffddeeff)
+          ),
+        ],
+        Colors.green.withOpacity(COLORBOX_OPACITY),
+      ),
+
+      ColorBox(
+        [
+          Heading("Endgame"),
+          park,
+          climb,
+          trap,
+        ],
+        Colors.blue.withOpacity(COLORBOX_OPACITY),
+      ),
+
+      ColorBox(
+        [
+          Heading("Match"),
+          Boolean("Defense Played"),
+          Numeric("Driver Skill", max: BigInt.from(10)),
+          Numeric("Fouls"),
+          Numeric("Tech Fouls"),
+          Boolean("Died"),
+          StringInput("Notes"),
+        ],
+        Colors.purple.withOpacity(COLORBOX_OPACITY),
+      ),
+      // Teleop cycles
+      Calculated(() => (
+        (teleopSpeaker.num ?? BigInt.zero) +
+        (teleopAmp.num ?? BigInt.zero) +
+        (teleopAmpSpeaker.num ?? BigInt.zero)
+      ).toString()),
+      // Total points
+      Calculated(() => (
+        (mobility.enabled ? BigInt.two : BigInt.zero) +
+        (autoSpeaker.num ?? BigInt.zero) * BigInt.from(5) +
+        (autoAmp.num ?? BigInt.zero) * BigInt.two +
+
+        (teleopSpeaker.num ?? BigInt.zero) * BigInt.two +
+        (teleopAmp.num ?? BigInt.zero) +
+        (teleopAmpSpeaker.num ?? BigInt.zero) * BigInt.from(5) +
+
+        (climb.enabled ? BigInt.from(3) : BigInt.zero) +
+        (trap.num ?? BigInt.zero) * BigInt.from(5) +
+        (park.enabled ? BigInt.one : BigInt.zero)
+      ).toString()),
+      Hidden(team),
+      Hidden(teamsideNotes),
+      Hidden(middleNotes),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!initialized) {
+      loadCrescendo();
+      initialized = true;
+    }
     List<Widget> stats = <Widget>[];
+    //loadChargedUp();
     for (var stat in statistics) {
       stat.setUpdate(update);
-      var widget = stat.widget();
+      var widget = stat.widget(context);
       if (widget != null) {
         stats.add(widget);
       }
@@ -556,13 +922,14 @@ class _MyHomePageState extends State<MyHomePage> {
                 statistics
                   .map((e) => e.get())
                   .where((element) => element != null)
-                  .join(DELIMITER)
-              )
+                  .join(DELIMITER),
+                teamNum.get() ?? "!No team number!"
+              ),
             ),
           );
         },
-        child: Text("View QR")
-      )
+        child: const Text("View QR"),
+      ),
     );
     stats.add(
       TextButton(
@@ -572,8 +939,8 @@ class _MyHomePageState extends State<MyHomePage> {
             stat.reset();
           }
         },
-        child: Text("Reset")
-      )
+        child: const Text("Reset"),
+      ),
     );
 
     return Scaffold(
